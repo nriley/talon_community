8c60fa3fb45e7f9de4a09da2acfc5b03c471a8e2
Nicholas Riley <com-github@sabi.net>
2022-03-10 15:23:16 -0500

    Work around a bizarre issue with window AXSubroles changing when the app is hidden.

--- apps/platforms/mac/citrix/citrix_global.py
+++ apps/platforms/mac/citrix/citrix_global.py
@@ -17,7 +17,15 @@ class Action:
 class UserActions:
 	def citrix_focus_desktop():
 		for viewer in ui.apps(bundle='com.citrix.receiver.icaviewer.mac'):
+			# XXX work around the subrole being AXDialog when the app is hidden
+			was_hidden = viewer.element.AXHidden
+			if was_hidden is True:
+				viewer.element.AXHidden = False
 			for window in viewer.windows():
 				if window.element.AXSubrole == 'AXStandardWindow':
+					viewer.focus()
 					window.focus()
 					return
+			if was_hidden:
+				viewer.element.AXHidden = True
+
