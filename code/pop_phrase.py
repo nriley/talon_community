08d10560f29ebfda1d35d8874559922704f45151
Nicholas Riley <com-github@sabi.net>
2022-07-04 10:28:43 -0400

    Nope nope nope.

--- code/pop_phrase.py
+++ code/pop_phrase.py
@@ -8,7 +8,7 @@ mod = Module()
 last_phrase = ""
 pop_phrase = ""
 
-auto_pop_that_phrases = ["go", "move", "select", "undo that", "cursor more"]
+auto_pop_that_phrases = ["go", "nope", "move", "select", "undo that", "cursor more"]
 
 
 def on_phrase(j):
