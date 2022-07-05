3478d72dbfd73acc7fccfe9610d315bce9b096a5
Nicholas Riley <com-github@sabi.net>
2022-07-05 19:29:54 -0400

    Remove work around for microphone selection on Windows as selecting System Default works fine.

--- modes/win_microphone.py
+++ modes/win_microphone.py
@@ -6,28 +6,6 @@ ctx.matches = """
 os: windows
 """
 
-@mod.action_class
-class Actions:
-	def reselect_microphone():
-		"""Reselect microphone (workaround for Talon bug on Windows) if speech is active"""
-
-@Context().action_class('user')
-class FallbackUserActions:
-	def reselect_microphone(): pass
-
-@ctx.action_class('user')
-class UserActions:
-	def reselect_microphone():
-		if 'sleep' in scope.get('mode'):
-			return
-
-		active_microphone = actions.sound.active_microphone()
-		if active_microphone == "None":
-			return
-
-		actions.sound.set_microphone("None")
-		actions.sound.set_microphone(active_microphone)
-
 def ui_callback(event, app):
 	if event not in ('app_launch', 'app_close'):
 		return
