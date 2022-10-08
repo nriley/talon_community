0dc00d2668440af61ca0a593b6f329924c776c0d
Nicholas Riley <com-github@sabi.net>
2022-10-08 17:06:20 -0400

    Replace cffi support for CoreDockSendNotification with Talon built-in support.

--- apps/dock/dock.py
+++ apps/dock/dock.py
@@ -1,7 +1,5 @@
 from talon import Context, Module, actions, clip, ui
 
-from .dock_spi import ffi
-
 ctx = Context()
 mod = Module()
 
@@ -20,25 +18,9 @@ cf = None
 appservices = None
 
 
-def load():
-    global cf, appservices
-    if cf is not None:
-        return
-
-    cf = ffi.dlopen(
-        "/System/Library/Frameworks/CoreFoundation.framework/CoreFoundation"
-    )
-    appservices = ffi.dlopen(
-        "/System/Library/Frameworks/ApplicationServices.framework/ApplicationServices"
-    )
-
-
 @ctx.action_class("user")
 class UserActions:
     def dock_send_notification(notification: str):
-        load()
-        notification_cfstr = cf.CFStringCreateWithCString(
-            ffi.NULL, notification.encode("utf8"), cf.kCFStringEncodingUTF8
-        )
-        appservices.CoreDockSendNotification(notification_cfstr, ffi.NULL)
-        cf.CFRelease(notification_cfstr)
+        from talon.mac.dock import dock_notify
+
+        dock_notify(notification)
