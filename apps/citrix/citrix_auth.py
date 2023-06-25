68ae2554e2e027b4a235b440d03c657aaedcf210
Nicholas Riley <com-github@sabi.net>
2023-06-25 11:57:42 -0400

    Remove workaround for Talon not noticing windows.
    (Core Talon issue fixed: https://github.com/talonvoice/talon/issues/315)

--- apps/citrix/citrix_auth.py
+++ apps/citrix/citrix_auth.py
@@ -12,21 +12,7 @@ and app.bundle: com.citrix.AuthManagerMac
 @ctx.action_class("user")
 class UserActions:
     def citrix_sign_in():
-        citrix_auth = ui.apps(bundle="com.citrix.AuthManagerMac")[0]
-        # XXX have to focus another app then back to the app or the window will not show up
-        ui.apps(bundle="com.apple.finder")[0].focus()
-        actions.sleep("100ms")
-        citrix_auth.focus()
-        for attempt in range(10):
-            windows = citrix_auth.windows()
-            if len(windows) == 0:
-                actions.sleep("100ms")
-                continue
-            window = citrix_auth.windows()[0]
-            break
-        else:
-            app.notify("Gave up while waiting for window")
-            return
+        window = ui.apps(bundle="com.citrix.AuthManagerMac")[0].active_window
 
         # Log in with web view (makes some assumptions about form elements)
         try:
