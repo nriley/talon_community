72cbbb1e124f813794ee08d1f161c2ef542294cc
Nicholas Riley <com-github@sabi.net>
2023-08-07 18:16:52 -0400

    Zoom to fit in PowerPoint for Mac.

--- apps/powerpoint/powerpoint_mac.py
+++ apps/powerpoint/powerpoint_mac.py
@@ -7,7 +7,14 @@ ctx.matches = r"""
 app: powerpoint_mac
 """
 
+
 @ctx.action_class("app")
 class AppActions:
     def window_open():
         actions.user.menu_select("Window|New Window")
+
+
+@ctx.action_class("user")
+class UserActions:
+    def zoom_to_fit():
+        actions.key("cmd-alt-o")
