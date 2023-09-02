8a42e926acd42ddbd2a9bed14de725c754800a2a
Nicholas Riley <com-github@sabi.net>
2023-09-02 12:20:21 -0400

    Improve reliability of Moom by working around flaky win_focus handler.
    Instead, specifically look for windows that match the Moom window we are wanting to see.

--- apps/moom/moom.py
+++ apps/moom/moom.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 from typing import Union
 
-from talon import Module, actions, ui
+from talon import Module, actions, cron, ui
 
 mod = Module()
 
@@ -10,7 +10,7 @@ def moom_app():
     return ui.apps(bundle="com.manytricks.Moom")[0]
 
 
-focused_handler = None
+opened_handler = None
 closed_handler = None
 moom_controls = []
 
@@ -27,51 +27,57 @@ class MoomControl:
 MOOM_DISMISS = MoomControl("return", dismisses=True)
 
 
-def moom_controls_if_focused():
-    global focused_handler
+def moom_controls_if_opened():
+    global opened_handler
 
-    def win_focused(win):
-        global focused_handler, closed_handler, moom_controls
-        ui.unregister("win_focus", focused_handler)
-        focused_handler = None
+    def win_opened(win):
+        global opened_handler, closed_handler, moom_controls
 
-        if win.app.bundle == "com.manytricks.Moom":
-            for i, control in enumerate(moom_controls):
-                if control.dismisses:
-                    dismissing_controls = moom_controls[: i + 1]
-                    del moom_controls[: i + 1]
-                    break
-            else:
-                dismissing_controls = moom_controls + [MOOM_DISMISS]
-                moom_controls = []
+        if win.app.bundle != "com.manytricks.Moom":
+            return
 
-            actions.key(" ".join(map(str, dismissing_controls)))
-            if moom_controls:
+        if win.element.AXSubrole != "AXSystemDialog":
+            return
 
-                def win_closed(closed_win):
-                    global closed_handler
-                    if win != closed_win:
-                        return
-                    ui.unregister("win_close", closed_handler)
-                    closed_handler = None
-                    moom_controls_if_focused()
-                    actions.key("ctrl-alt-m")
+        ui.unregister("win_open", opened_handler)
+        opened_handler = None
 
-                closed_handler = win_closed
-                ui.register("win_close", closed_handler)
-                return
+        for i, control in enumerate(moom_controls):
+            if control.dismisses:
+                dismissing_controls = moom_controls[: i + 1]
+                del moom_controls[: i + 1]
+                break
+        else:
+            dismissing_controls = moom_controls + [MOOM_DISMISS]
+            moom_controls = []
 
-    focused_handler = win_focused
-    ui.register("win_focus", win_focused)
+        actions.key(" ".join(map(str, dismissing_controls)))
+        if moom_controls:
+
+            def win_closed(closed_win):
+                global closed_handler
+                if win != closed_win:
+                    return
+                ui.unregister("win_close", closed_handler)
+                closed_handler = None
+                moom_controls_if_opened()
+                actions.key("ctrl-alt-m")
+
+            closed_handler = win_closed
+            ui.register("win_close", closed_handler)
+            return
+
+    opened_handler = win_opened
+    ui.register("win_open", win_opened)
 
 
 @mod.action_class
 class Actions:
     def moom(key: Union[str, MoomControl]):
         """Press the corresponding Moom control [key]"""
-        global focused_handler, moom_controls
-        if not focused_handler:
-            moom_controls_if_focused()
+        global opened_handler, moom_controls
+        if not opened_handler:
+            moom_controls_if_opened()
             actions.key("ctrl-alt-m")
         moom_controls.append(
             MoomControl(key, dismisses=True) if isinstance(key, str) else key
