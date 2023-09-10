a1211d1873ac3d57dc81a79edafa0c1fac60dcc2
Nicholas Riley <com-github@sabi.net>
2023-09-10 14:17:51 -0400

    Replace majority of RStudio Mac implementation with standard commands/actions.

---
+++ apps/rstudio/rstudio_mac.py
@@ -0,0 +1,116 @@
+from talon import Context, Module, actions, app, ui
+
+mod = Module()
+ctx = Context()
+
+mod.apps.rstudio = r"""
+os: mac
+and app.bundle: com.rstudio.desktop
+"""
+
+ctx.matches = r"""
+os: mac
+app: rstudio
+"""
+
+
+@ctx.action_class("app")
+class AppActions:
+    def tab_open():
+        actions.key("cmd-shift-n")
+
+    def tab_final():
+        actions.key("ctrl-shift-f12")
+
+    def tab_next():
+        actions.key("ctrl-f12")
+
+    def tab_previous():
+        actions.key("ctrl-f11")
+
+
+@ctx.action_class("code")
+class CodeActions:
+    def toggle_comment():
+        actions.key("cmd-shift-c")
+
+
+@ctx.action_class("edit")
+class EditActions:
+    def jump_line(n):
+        actions.key("cmd-shift-alt-g")
+        actions.sleep("100ms")
+        actions.insert(n)
+        actions.key("enter")
+        actions.edit.line_start()
+
+    def line_clone():
+        actions.key("cmd-alt-down")
+
+
+@ctx.action_class("user")
+class UserActions:
+    # user.find_and_replace
+    def find(text: str):
+        actions.key("cmd-f")
+        if text:
+            actions.key("cmd-a")
+            actions.user.paste(text)
+
+    def find_next():
+        actions.key("cmd-g")
+
+    def find_previous():
+        actions.key("cmd-shift-g")
+
+    def find_everywhere(text: str):
+        actions.key("cmd-shift-f")
+
+    def find_toggle_match_by_case():
+        actions.user.find("")
+        actions.key("tab:8 space")
+
+    def find_toggle_match_by_word():
+        actions.user.find("")
+        actions.key("tab:9 space")
+
+    def find_toggle_match_by_regex():
+        actions.user.find("")
+        actions.key("tab:10 space")
+
+    replace = find
+    replace_everywhere = find_everywhere
+
+    def replace_confirm():
+        actions.key("cmd-shift-j")
+
+    def replace_confirm_all():
+        actions.user.find("")
+        actions.key("tab:6 space")
+
+    def select_previous_occurrence(text: str):
+        # Can't entirely suppress the find pane but at least hide it
+        actions.user.find(text)
+        actions.user.find_previous()
+        actions.key("esc")
+
+    def select_next_occurrence(text: str):
+        # Can't entirely suppress the find pane but at least hide it
+        actions.user.find(text)
+        actions.user.find_next()
+        actions.key("esc")
+
+    # user.line_commands
+
+    # user.multiple_cursors
+    def multi_cursor_disable():
+        actions.key("esc")
+
+    def multi_cursor_add_above():
+        actions.key("ctrl-alt-up")
+
+    def multi_cursor_add_below():
+        actions.key("ctrl-alt-down")
+
+    def multi_cursor_add_to_line_ends():
+        actions.key("ctrl-alt-a")
