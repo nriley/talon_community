13fe2c50e5d4d2a410f3ac88a19cbeebf0d75330
Nicholas Riley <com-github@sabi.net>
2024-02-29 21:15:16 -0500

    Make "focus" on Windows work again.

--- apps/dock/dock.talon
+++ apps/dock/dock.talon
@@ -1,4 +1,5 @@
-^focus$: user.dock_send_notification("com.apple.expose.awake")
+os: mac
+-
 ^desktop$: user.dock_send_notification("com.apple.showdesktop.awake")
 ^window$: user.dock_send_notification("com.apple.expose.front.awake")
 ^launch pad$: user.dock_send_notification("com.apple.launchpad.toggle")
--- core/app_switcher/app_switcher.py
+++ core/app_switcher/app_switcher.py
@@ -405,6 +405,8 @@ class Actions:
         """Open a menu of running apps to switch to"""
         if app.platform == "windows":
             actions.key("alt-ctrl-tab")
+        elif app.platform == "mac":
+            actions.user.dock_send_notification("com.apple.expose.awake")
         else:
             print("Persistent Switcher Menu not supported on " + app.platform)
 
