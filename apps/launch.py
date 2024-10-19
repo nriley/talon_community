8f74ec9d344f68f30ef03f168c385273260eabd7
Nicholas Riley <com-github@sabi.net>
2024-10-19 16:39:27 -0400

    Handle launching/"focusing" Mac apps that never become frontmost.

--- apps/launch.py
+++ apps/launch.py
@@ -3,6 +3,22 @@ from talon import Module, actions, app, ui
 mod = Module()
 
 
+def wait_focused_app(bundle, tries=10):
+    for attempt in range(tries):
+        active_app = ui.active_app()
+        if active_app.bundle == bundle:
+            return active_app
+        try:
+            active_app = ui.focused_element().window.app
+            if active_app.bundle == bundle:
+                return active_app
+        except (RuntimeError, OSError, ui.UIErr):
+            pass
+        actions.sleep("50ms")
+    else:
+        return None
+
+
 @mod.action_class
 class Actions:
     def launch_bundle(bundle: str):
@@ -18,23 +34,18 @@ class Actions:
             ui.apps(bundle=bundle)[0].focus()
         except IndexError:
             return None
-        for attempt in range(10):
-            active_app = ui.active_app()
-            if active_app.bundle == bundle:
-                return active_app
-            actions.sleep("50ms")
-        app.notify(title="Failed to focus application", body=f"Bundle ID: {bundle}")
-        return None
+
+        if (focused_app := wait_focused_app(bundle)) is None:
+            app.notify(title="Failed to focus application", body=f"Bundle ID: {bundle}")
+        return focused_app
 
     def launch_or_focus_bundle(bundle: str):
         """Launch or focus and return an application by bundle ID, or None if unable to do so."""
-        app = actions.user.focus_bundle(bundle)
-        if app is not None:
-            return app
+        if (focused_app := actions.user.focus_bundle(bundle)) is not None:
+            return focused_app
+
         actions.user.launch_bundle(bundle)
-        for attempt in range(100):
-            active_app = ui.active_app()
-            if active_app.bundle == bundle:
-                return active_app
-            actions.sleep("50ms")
+        if (focused_app := wait_focused_app(bundle, tries=100)) is not None:
+            return focused_app
+
         return actions.user.focus_bundle(bundle)
