a0cff6d11c0319f02a5d74f082f46cf41368e33d
Nicholas Riley <com-github@sabi.net>
2024-10-26 11:10:56 -0400

    Go to next/previous channel across workspaces in Shrugs.

--- apps/shrugs/shrugs_mac.py
+++ apps/shrugs/shrugs_mac.py
@@ -27,11 +27,59 @@ def channel_outline():
     )
 
 
+def workspace_table():
+    return (
+        shrugs()
+        .active_window.element.children.find_one(AXRole="AXSplitGroup", max_depth=0)
+        .children.find_one(AXIdentifier="SlickServicesIconPaneVC", max_depth=0)
+        .children.find_one(AXRole="AXTable", max_depth=0)
+    )
+
+
 @mod.action_class
 class Actions:
     def shrugs_channel_reload():
         """Go to the next and then previous channel in order to work around
         a Shrugs bug in which the channel contents do not appear"""
+
+
+def go_unread(go_next=True):
+    workspaces = None
+
+    while True:
+        channels = channel_outline()
+        selected_row_index = channels.AXSelectedRows[0].AXIndex
+        channel_rows = channels.AXRows
+        if not go_next:
+            channel_rows = reversed(channel_rows)
+        for row in channel_rows:
+            if go_next:
+                if row.AXIndex <= selected_row_index:
+                    continue
+            elif row.AXIndex >= selected_row_index:
+                continue
+            if len(row.children[0].children) > 2:
+                row.AXSelected = True
+                return True
+        # Server unread indicator/count is not exposed via accessibility
+        if workspaces is None:
+            workspaces = workspace_table()
+            workspace_rows = workspaces.AXRows
+            original_workspace_index = workspaces.AXSelectedRows[0].AXIndex
+            selected_row_index = original_workspace_index
+        else:
+            selected_row_index = workspaces.AXSelectedRows[0].AXIndex
+        if go_next:
+            selected_row_index += 1
+            if selected_row_index >= len(workspace_rows):
+                selected_row_index = original_workspace_index
+        else:
+            selected_row_index -= 1
+            if selected_row_index < 0:
+                selected_row_index = original_workspace_index
+        workspace_rows[selected_row_index].AXSelected = True
+        if selected_row_index == original_workspace_index:
+            return False
 
 
 @ctx.action_class("user")
@@ -56,24 +104,24 @@ class UserActions:
     # Navigation: Channels
     def messaging_channel_previous():
         # XXX DM support
-        outline = channel_outline()
-        selected_row_index = outline.AXSelectedRows[0].AXIndex
+        channels = channel_outline()
+        selected_row_index = channels.AXSelectedRows[0].AXIndex
         if selected_row_index == 1:
             app.notify(
                 title="Shrugs: No previous channel",
                 body="This is already the first channel",
             )
             return
-        prev_row = outline.children.find_one(
+        prev_row = channels.children.find_one(
             AXRole="AXRow", AXIndex=selected_row_index - 1, max_depth=0
         )
         prev_row.AXSelected = True
 
     def messaging_channel_next():
-        outline = channel_outline()
-        selected_row_index = outline.AXSelectedRows[0].AXIndex
+        channels = channel_outline()
+        selected_row_index = channels.AXSelectedRows[0].AXIndex
         try:
-            next_row = outline.children.find_one(
+            next_row = channels.children.find_one(
                 AXRole="AXRow", AXIndex=selected_row_index + 1, max_depth=0
             )
         except ui.UIErr:
@@ -85,25 +133,9 @@ class UserActions:
         next_row.AXSelected = True
 
     def messaging_unread_previous():
-        outline = channel_outline()
-        selected_row_index = outline.AXSelectedRows[0].AXIndex
-        for row in reversed(outline.AXRows):
-            if row.AXIndex >= selected_row_index:
-                continue
-            if len(row.children[0].children) > 2:
-                row.AXSelected = True
-                return
-        # XXX Go to previous unread server
-        app.notify(title="Shrugs: No previous unread channel")
+        if not go_unread(go_next=False):
+            app.notify(title="Shrugs: No previous unread channel")
 
     def messaging_unread_next():
-        outline = channel_outline()
-        selected_row_index = outline.AXSelectedRows[0].AXIndex
-        for row in outline.AXRows:
-            if row.AXIndex <= selected_row_index:
-                continue
-            if len(row.children[0].children) > 2:
-                row.AXSelected = True
-                return
-        # XXX Go to next unread server
-        app.notify(title="Shrugs: No next unread channel")
+        if not go_unread():
+            app.notify(title="Shrugs: No next unread channel")
