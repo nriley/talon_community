5d4261261f4ae0ad0ac43cf52363e25c886281b1
Nicholas Riley <com-github@sabi.net>
2024-11-23 21:51:16 -0500

    Changes from #1610.

--- apps/dock/dock.py
+++ apps/dock/dock.py
@@ -1,3 +1,6 @@
+from pathlib import Path
+from typing import Optional
+
 from talon import Context, Module, actions, clip, ui
 
 ctx = Context()
@@ -13,16 +16,17 @@ class Actions:
     def dock_send_notification(notification: str):
         """Send a CoreDock notification to the macOS Dock using SPI"""
 
-    def dock_app_expose():
-        """Activate app Exposé using the macOS Dock item for the frontmost app"""
+    def dock_app_expose(app: Optional[ui.App] = None):
+        """Activate macOS app Exposé via its Dock item (for the frontmost app if not specified)"""
 
 
 @ctx.action_class("user")
 class UserActions:
-    def dock_app_expose():
-        from pathlib import Path
+    def dock_app_expose(app=None):
+        if app is None:
+            app = ui.active_app()
 
-        app_name = Path(ui.active_app().path).stem
+        app_name = Path(app.path).stem
         dock_items = ui.apps(bundle="com.apple.dock")[0].children.find(
             AXSubrole="AXApplicationDockItem", AXTitle=app_name, max_depth=1
         )
--- apps/dock/dock.talon
+++ apps/dock/dock.talon
@@ -2,4 +2,7 @@ os: mac
 -
 ^desktop$: user.dock_send_notification("com.apple.showdesktop.awake")
 ^window$: user.dock_app_expose()
+^window <user.running_applications>$:
+    app = user.get_running_app(running_applications)
+    user.dock_app_expose(app)
 ^launch pad$: user.dock_send_notification("com.apple.launchpad.toggle")
