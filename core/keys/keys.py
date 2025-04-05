6d5be3762866dd9db15d0c46827cd67c76878d2a
Jeff Knaus <knaus.jeff@gmail.com>
2025-04-05 10:47:55 -0600

    Move symbols into their own python file (#1763)
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Nicholas Riley <com-github@sabi.net>

--- core/keys/keys.py
+++ core/keys/keys.py
@@ -1,6 +1,19 @@
 from talon import Context, Module, actions, app
 
+from .symbols import (
+    dragon_punctuation_dict,
+    punctuation_dict,
+    symbol_key_dict,
+)
+
 mod = Module()
+ctx = Context()
+
+ctx_dragon = Context()
+ctx_dragon.matches = r"""
+speech.engine: dragon
+"""
+
 mod.list("letter", desc="The spoken phonetic alphabet")
 mod.list("symbol_key", desc="All symbols from the keyboard")
 mod.list("arrow_key", desc="All arrow keys")
@@ -103,104 +116,13 @@ def letters(m) -> str:
     return "".join(m.letter_list)
 
 
-ctx = Context()
+@mod.action_class
+class Actions:
+    def get_punctuation_words():
+        """Gets the user.punctuation list"""
+        return punctuation_dict
 
-# `punctuation_words` is for words you want available BOTH in dictation and as key names in command mode.
-# `symbol_key_words` is for key names that should be available in command mode, but NOT during dictation.
-punctuation_words = {
-    "back tick": "`",
-    "comma": ",",
-    "come a": ",",
-    # Workaround for issue with conformer b-series; see #946
-    "coma": ",",
-    "period": ".",
-    "semicolon": ";",
-    "colon": ":",
-    "dash": "-",
-    "m dash": "—",
-    "en dash": "–",
-    "slash": "/",
-    "question mark": "?",
-    "exclamation mark": "!",
-    "exclamation point": "!",
-    "asterisk": "*",
-    "hash sign": "#",
-    "number sign": "#",
-    "percent sign": "%",
-    "at sign": "@",
-    "and sign": "&",
-    "ampersand": "&",
-    "hyphen": "-",
-    "left paren": "(",
-    "open paren": "(",
-    "open parenthesis": "(",
-    "right paren": ")",
-    "close paren": ")",
-    "close parenthesis": ")",
-    "open bracket": "[",
-    "close bracket": "]",
-    # Currencies
-    "dollar sign": "$",
-    "pound sign": "£",
-}
-symbol_key_words = {
-    "semi": ";",
-    "dot": ".",
-    "point": ".",
-    "quote": "'",
-    "question": "?",
-    "apostrophe": "'",
-    # "L square": "[",
-    # "left square": "[",
-    # "brack": "[",
-    # "bracket": "[",
-    "square": "[",
-    "R square": "]",
-    "right square": "]",
-    # "r brack": "]",
-    # "r bracket": "]",
-    "slash": "/",
-    "backslash": "\\",
-    "whack": "\\",
-    "minus": "-",
-    "equals": "=",
-    "plus": "+",
-    "grave": "`",
-    "tilde": "~",
-    "bang": "!",
-    "underscore": "_",
-    "score": "_",
-    "paren": "(",
-    "brace": "{",
-    "left brace": "{",
-    "curly bracket": "{",
-    "left curly bracket": "{",
-    "r brace": "}",
-    "right brace": "}",
-    "r curly bracket": "}",
-    "right curly bracket": "}",
-    "angle": "<",
-    "left angle": "<",
-    "less than": "<",
-    "rangle": ">",
-    "R angle": ">",
-    "right angle": ">",
-    "greater than": ">",
-    "star": "*",
-    "number": "#",
-    "pound": "#",
-    "hash": "#",
-    "percent": "%",
-    "caret": "^",
-    "amper": "&",
-    "pipe": "|",
-    "dub quote": '"',
-    "double quote": '"',
-    # Currencies
-    "dollar": "$",
-}
 
-# make punctuation words also included in {user.symbol_keys}
-symbol_key_words.update(punctuation_words)
-ctx.lists["self.punctuation"] = punctuation_words
-ctx.lists["self.symbol_key"] = symbol_key_words
+ctx.lists["user.punctuation"] = punctuation_dict
+ctx.lists["user.symbol_key"] = symbol_key_dict
+ctx_dragon.lists["user.punctuation"] = dragon_punctuation_dict
