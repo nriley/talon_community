7752f351b22213f5a4f88d5a943eeac8fdcca0bd
Nicholas Riley <com-github@sabi.net>
2025-04-26 12:15:40 -0400

    Fix incorrect commit to one matching #1825.

--- core/keys/symbols.py
+++ core/keys/symbols.py
@@ -64,7 +64,7 @@ symbols = [
     Symbol("*", ["asterisk"], ["star"]),
     Symbol("#", ["number sign"], ["number"]),
     Symbol("%", ["percent sign"], ["percent"]),
-    Symbol("@", ["atsign"], ["at sign"]),
+    Symbol("@", ["at symbol"], ["at sign"]),
     Symbol("&", ["ampersand"]),
     Symbol("-", ["hyphen"], ["minus", "dash"]),
     Symbol("–", ["en dash"]),
