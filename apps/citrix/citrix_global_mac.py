c9265c8dd4a3b82e0c5871896cdbddbae1ebf10a
Nicholas Riley <com-github@sabi.net>
2025-06-19 16:40:47 -0400

    Actions to focus a Citrix app husk on macOS.

--- apps/citrix/citrix_global_mac.py
+++ apps/citrix/citrix_global_mac.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 from talon import Context, Module, actions, ui
 
 mod = Module()
@@ -15,8 +17,14 @@ class Actions:
     def citrix_focus_workspace():
         """Focus Citrix Workspace"""
 
-    def citrix_launch_favorite(citrix_favorite: str):
+    def citrix_launch_favorite(favorite: str):
         """Launch the specified Citrix app or desktop"""
+
+    def citrix_focus_app(app_name: str) -> bool:
+        """Focus a Citrix app, returning whether successful"""
+
+    def citrix_launch_or_focus(favorite: str):
+        """Focus a Citrix app if running, otherwise launch it"""
 
 
 @ctx.action_class("user")
@@ -41,3 +49,29 @@ class UserActions:
 
     def citrix_focus_workspace():
         actions.user.launch_or_focus_bundle("com.citrix.receiver.nomas")
+
+    def citrix_focus_app(app_name):
+        for husk in ui.apps(bundle="com.citrix.ctxapphusk"):
+            husk_name = Path(husk.exe).parents[2].stem
+            if app_name.startswith(husk_name):
+                actions.user.switcher_save_mouse_pos()
+                husk.focus()
+                for attempt in range(10):
+                    actions.sleep("10ms")
+                    active_app = ui.active_app()
+                    if active_app.bundle == "com.citrix.receiver.icaviewer.mac":
+                        break
+                else:
+                    raise ("Timed out waiting to focus Citrix Viewer")
+                    return False
+                actions.user.switcher_restore_mouse_pos(ui.active_app())
+        else:
+            return False
+
+        return True
+
+    def citrix_launch_or_focus(favorite):
+        if actions.user.citrix_focus_app(favorite):
+            return
+
+        actions.user.citrix_launch_favorite(favorite)
