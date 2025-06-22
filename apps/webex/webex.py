b412e393ab45c3154f7cabbb0f4927f3fd51194d
Nicholas Riley <com-github@sabi.net>
2025-06-21 23:19:27 -0400

    Update meeting support for current Mac Webex app.

--- apps/webex/webex.py
+++ apps/webex/webex.py
@@ -9,69 +9,74 @@ tag: user.meeting_webex
 os: mac
 """
 
-WEBEX_MEETINGS_BUNDLE_ID = "com.webex.meetingmanager"
+WEBEX_BUNDLE_ID = "Cisco-Systems.Spark"
 
 
-def is_webex_meetings(app):
-    return app.bundle == WEBEX_MEETINGS_BUNDLE_ID
+def is_webex(app):
+    return app.bundle == WEBEX_BUNDLE_ID
 
 
-def webex_meetings_app():
-    if apps := ui.apps(bundle=WEBEX_MEETINGS_BUNDLE_ID):
+def webex_app():
+    if apps := ui.apps(bundle=WEBEX_BUNDLE_ID):
         return apps[0]
     return None
 
 
-def webex_meetings_window_is_meeting_window(window):
-    return window.title == "Webex" or window.element.get("AXIdentifier") in (
-        "Acc_FIT_Win",
-        "Acc_FloatingWidget_Win",
+def webex_window_is_meeting_window(window):
+    return window.element.get("AXIdentifier") in (
+        "PopOutWindow",
+        "FloatingMediaWindow",
     )
 
 
-def webex_meetings_window():
-    if webex_meetings := webex_meetings_app():
-        for window in webex_meetings.windows():
-            if webex_meetings_window_is_meeting_window(window):
+def webex_window():
+    if webex := webex_app():
+        for window in webex.windows():
+            if webex_window_is_meeting_window(window):
                 return window
     return None
 
 
 def is_webex_meeting_window(window):
-    return is_webex_meetings(window.app) and webex_meetings_window_is_meeting_window(
-        window
-    )
+    return is_webex(window.app) and webex_window_is_meeting_window(window)
 
 
 def webex_toggle_mute():
-    if webex_meetings := webex_meetings_app():
-        ctrl.key_press("m", super=True, shift=True, app=webex_meetings)
+    if webex := webex_app():
+        ctrl.key_press("m", super=True, shift=True, app=webex)
 
 
 def on_win_open(window):
     if is_webex_meeting_window(window):
         actions.user.meeting_started("webex", window)
+        ui.register("win_focus", on_win_focus)
 
 
-def on_win_close(window):
+def on_win_focus(window):
     if is_webex_meeting_window(window):
-        actions.user.meeting_ended("webex", window)
+        return
+
+    if webex_window():
+        return
+
+    actions.user.meeting_ended("webex", window)
+    ui.unregister("win_focus", on_win_focus)
 
 
 def on_ready():
-    if meeting_window := webex_meetings_window():
+    if meeting_window := webex_window():
         actions.user.meeting_started("webex", meeting_window)
+        ui.register("win_focus", on_win_focus)
 
     ui.register("win_open", on_win_open)
-    ui.register("win_close", on_win_close)
 
 
 @ctx.action_class("user")
 class UserActions:
     def meeting_is_muted() -> bool:
-        if webex_meetings := webex_meetings_app():
+        if webex := webex_app():
             try:
-                mute_menu_item = webex_meetings.element.children.find_one(
+                mute_menu_item = webex.element.children.find_one(
                     AXRole="AXMenuBar", max_depth=0
                 ).children.find_one(
                     AXRole="AXMenuItem", AXMenuItemCmdChar="M", AXMenuItemCmdModifiers=1
@@ -97,8 +102,8 @@ class UserActions:
         webex_toggle_mute()
 
     def meeting_exit():
-        if webex_meetings := webex_meetings_app():
-            webex_meetings.quit()
+        if webex := webex_app():
+            webex.quit()
 
 
 app.register("ready", on_ready)
