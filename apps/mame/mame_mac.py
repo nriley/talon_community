23c4b91be16ce657176930b219a582c8aad96f51
Nicholas Riley <com-github@sabi.net>
2025-07-04 21:39:41 -0400

    Very minimal implementation for MAME on Mac.

---
+++ apps/mame/mame_mac.py
@@ -0,0 +1,14 @@
+from talon import Context, Module, actions
+
+mod = Module()
+ctx = Context()
+
+mod.apps.mame = r"""
+os: mac
+and app.bundle: org.mamedev.mame
+"""
+
+ctx.matches = r"""
+os: mac
+app: mame
+"""
---
+++ apps/mame/mame_mac.talon
@@ -0,0 +1,6 @@
+os: mac
+app: mame
+-
+app quit: key(cmd-alt-q)
+
+capture: user.menu_select("Special|Capture Mouse")
